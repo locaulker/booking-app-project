0a0cfd6fefaddb02b7197bd62f98c8ba52bf0a47
Lawrence <locaulker@gmail.com>
2021-03-05 00:45:09 -0500

    installed and configured firebase

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "83f2dd399e86cb672275";
+/******/ 	var hotCurrentHash = "65031132b4518bb6bd2d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ client/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC3_mPQdP0M7kD5V033bkSGuZkdz_AImNc",
+  authDomain: "booking-app-project-42e4d.firebaseapp.com",
+  projectId: "booking-app-project-42e4d",
+  storageBucket: "booking-app-project-42e4d.appspot.com",
+  messagingSenderId: "537691472069",
+  appId: "1:537691472069:web:c021aaaded1a74793c30ac"
+}
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
